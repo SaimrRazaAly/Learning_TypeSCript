51fa153de4b3ba0c185ff3524b527bb2d0aae23a
SaimRazaAly <saimrazateach2010@gmail.com>
2024-11-28 17:02:20 -0800

    today i learned about union, literal day type and more

--- day-1.js
+++ day-1.js
@@ -1,7 +1,3 @@
-var name = "saim";
-var age = 10;
-var isStudent = true;
-console.log("my name is ".concat(name, " and i am ").concat(age, " years old and this is ").concat(isStudent));
 function findSquare(num) {
     return num * num;
 }
@@ -20,3 +16,27 @@ console.log(add(1, 2));
         console.log("the enter number was a odd number");
     }
 })(2);
+// today we will learn about the concept of data type in ts
+//  EX :1 Define Variables with Annotations
+// Create variables for a person’s name, age, and whether they are married (use string, number, and boolean).
+var username = "saim";
+var userage = 20;
+var married = false;
+// EX 2 Array of Numbers and Strings
+// Create an array of your favorite numbers and an array of your favorite colors.
+var numbers = [2, 4, 6, 8];
+var colors = ["red", "orange", "yellow"];
+// Ex 2 Union Type Example
+// Write a function formatValue that takes a parameter of type string | number and returns a formatted string.
+// Example:
+(function LearningUnion(input1) {
+    return "the value you enterd was ".concat(input1);
+})(2);
+// EX 4 Literal Type for Days
+// Create a variable that can hold one of the days of the week ("Monday" | "Tuesday" | ...).
+var week;
+var newdate = new Date();
+var day = newdate.getDay;
+console.log(day);
+week = " Wednesday";
+console.log("today is ".concat(week));
--- day-1.ts
+++ day-1.ts
@@ -1,10 +1,3 @@
-let name = "saim";
-let age = 10;
-let isStudent = true;
-console.log(
-  `my name is ${name} and i am ${age} years old and this is ${isStudent}`
-);
-
 function findSquare(num: number): number {
   return num * num;
 }
@@ -25,5 +18,42 @@ console.log(add(1, 2));
   } else {
     console.log("the enter number was a odd number");
   }
-}
-)(2);
+})(2);
+
+// today we will learn about the concept of data type in ts
+//  EX :1 Define Variables with Annotations
+
+// Create variables for a person’s name, age, and whether they are married (use string, number, and boolean).
+
+let username: string = "saim";
+let userage: number = 20;
+let married: boolean = false;
+
+// EX 2 Array of Numbers and Strings
+// Create an array of your favorite numbers and an array of your favorite colors.
+
+let numbers:number[] = [2, 4, 6, 8];
+let colors: string[] = ["red", "orange", "yellow"];
+
+// Ex 2 Union Type Example
+
+// Write a function formatValue that takes a parameter of type string | number and returns a formatted string.
+// Example:
+
+(function LearningUnion(input1: number | string): string {
+  return `the value you enterd was ${input1}`;
+})(2);
+
+// EX 4 Literal Type for Days
+// Create a variable that can hold one of the days of the week ("Monday" | "Tuesday" | ...).
+
+let week:
+  | "Monday"
+  | "Tuesday"
+  | " Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+week = " Wednesday";
+console.log(`today is ${week}`);
